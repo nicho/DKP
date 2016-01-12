49332c76546b7fcd14e28b40e9ac325e07c9f707
Administrator <Administrator@A0L1IWVPICHCSDX>
2016-01-12 20:26:51 +0800

    11

--- src/main/java/com/gamewin/weixin/service/activity/ActivityService.java
+++ src/main/java/com/gamewin/weixin/service/activity/ActivityService.java
@@ -52,6 +52,7 @@ public class ActivityService {
 	private ActivityUserMybatisDao activityUserMybatisDao;
 	@Autowired(required = false)
 	private SpyMemcachedClient memcachedClient;
+
 	public String getAccessToken() {
 		String key = MemcachedObjectType.WEIXIN.getPrefix() + "AccessToken";
 
@@ -67,15 +68,16 @@ public class ActivityService {
 		}
 		return accessToken;
 	}
-		
+
 	public void updateActivityClose(Long id) {
-		Activity ac=activityDao.findOne(id);
+		Activity ac = activityDao.findOne(id);
 		if ("pass".equals(ac.getStatus())) {
 			ac.setStatus("close");
 			activityDao.save(ac);
 		}
-	
+
 	}
+
 	public Activity getActivity(Long id) {
 		return activityDao.findOne(id);
 	}
@@ -112,14 +114,20 @@ public class ActivityService {
 
 	public void saveActivityGHCreate(Activity entity) throws Exception {
 		User user = userDao.findOne(entity.getCreateUser().getId());
-		String integral3_1 = "≤300"; 
-		String integral3_2 = "≤400"; 
-		String integral3_3 = "≤500"; 
-		
+		String integral4_1 = "≤600";
+		String integral4_2 = "≤700";
+		String integral4_3 = "≤800";
+		String integral4_4 = "≤900";
+		String integral4_5 = "≤1000";
+
+		String integral3_1 = "≤300";
+		String integral3_2 = "≤400";
+		String integral3_3 = "≤500";
+
 		String integral1_1 = "≤10";
 		String integral1_2 = "≤20";
 		String integral1_3 = "≤30";
-		
+
 		String integral2_1 = "≤50";
 		String integral2_2 = "≤100";
 		String integral2_3 = "≤200";
@@ -133,47 +141,35 @@ public class ActivityService {
 		} else if ("ThreeLevel".equals(user.getRoles())) {
 			entity.setLevel(3);
 		}
-
-		if (integral3_1.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user
-						.getRoles()))) {
+		if (integral4_1.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()))) {
+			entity.setIntegral(600.0);
+		} else if (integral4_2.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()))) {
+			entity.setIntegral(700.0);
+		} else if (integral4_3.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()))) {
+			entity.setIntegral(800.0);
+		} else if (integral4_4.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()))) {
+			entity.setIntegral(900.0);
+		} else if (integral4_5.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()))) {
+			entity.setIntegral(1000.0);
+		} else if (integral3_1.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()))) {
 			entity.setIntegral(300.0);
-		}else if (integral3_2.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user
-						.getRoles()))) {
+		} else if (integral3_2.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()))) {
 			entity.setIntegral(400.0);
-		}else if (integral3_3.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user
-						.getRoles()))) {
+		} else if (integral3_3.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()))) {
 			entity.setIntegral(500.0);
-		}else if (integral2_1.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
+		} else if (integral2_1.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
 			entity.setIntegral(50.0);
-		} else if (integral2_2.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
+		} else if (integral2_2.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
 			entity.setIntegral(100.0);
-		} else if (integral2_3.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
+		} else if (integral2_3.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()))) {
 			entity.setIntegral(200.0);
-		} else if (integral1_1.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel"
-							.equals(user.getRoles()))) {
+		} else if (integral1_1.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel".equals(user.getRoles()))) {
 			entity.setIntegral(10.0);
-		} else if (integral1_2.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel"
-							.equals(user.getRoles()))) {
+		} else if (integral1_2.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel".equals(user.getRoles()))) {
 			entity.setIntegral(20.0);
-		}  else if (integral1_3.equals(entity.getPersonCount())
-				&& ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())
-						|| "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel"
-							.equals(user.getRoles()))) {
+		} else if (integral1_3.equals(entity.getPersonCount()) && ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles()) || "OneLevel".equals(user.getRoles()) || "TwoLevel".equals(user.getRoles()) || "ThreeLevel".equals(user.getRoles()))) {
 			entity.setIntegral(30.0);
-		}  else {
+		} else {
 			throw new Exception("用户权限不足,无法发布" + entity.getPersonCount() + "人公会活动");
 		}
 
@@ -187,9 +183,9 @@ public class ActivityService {
 		if (userList != null && userList.size() > 0) {
 			activityDao.save(entity);
 			Double integer = entity.getIntegral();
-			Double pInteger = integer / userList.size(); 
-	        BigDecimal bd = new BigDecimal(pInteger);  
-	        pInteger=bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();  //保留2位小数 
+			Double pInteger = integer / userList.size();
+			BigDecimal bd = new BigDecimal(pInteger);
+			pInteger = bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue(); // 保留2位小数
 			for (int i = 0; i < userList.size(); i++) {
 				User user = userList.get(i);
 				user.setIntegral(user.getIntegral() + pInteger);
@@ -223,8 +219,8 @@ public class ActivityService {
 			if (userList != null && userList.size() > 0) {
 				Double integer = entity.getIntegral();
 				Double pInteger = integer / userList.size();
-		        BigDecimal bd = new BigDecimal(pInteger);  
-		        pInteger=bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();  //保留2位小数 
+				BigDecimal bd = new BigDecimal(pInteger);
+				pInteger = bd.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue(); // 保留2位小数
 				for (int i = 0; i < userList.size(); i++) {
 					User user = userList.get(i);
 					user.setIntegral(user.getIntegral() + pInteger);
@@ -274,15 +270,14 @@ public class ActivityService {
 		this.activityDao = activityDao;
 	}
 
-	public Page<Activity> getAllActivity(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize,
-			String sortType) {
+	public Page<Activity> getAllActivity(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) {
 		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
 		Specification<Activity> spec = buildSpecification(userId, searchParams);
 
 		return activityDao.findAll(spec, pageRequest);
 	}
-	public Page<Activity> getPassActivity(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize,
-			String sortType) {
+
+	public Page<Activity> getPassActivity(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) {
 		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
 		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
 		filters.put("isdelete", new SearchFilter("isdelete", Operator.EQ, "0"));
@@ -291,8 +286,8 @@ public class ActivityService {
 
 		return activityDao.findAll(spec, pageRequest);
 	}
-	public Page<Activity> getAllProcessActivty(Long userId, Map<String, Object> searchParams, int pageNumber,
-			int pageSize, String sortType) throws Exception {
+
+	public Page<Activity> getAllProcessActivty(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) throws Exception {
 		User user = userDao.findOne(userId);
 
 		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
@@ -314,12 +309,11 @@ public class ActivityService {
 		return activityDao.findAll(spec, pageRequest);
 	}
 
-	public Page<Activity> getAllConfirmProcessActivty(Long userId, Map<String, Object> searchParams, int pageNumber,
-			int pageSize, String sortType) throws Exception {
+	public Page<Activity> getAllConfirmProcessActivty(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) throws Exception {
 		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
 		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
 		filters.put("isdelete", new SearchFilter("isdelete", Operator.EQ, "0"));
-		
+
 		User user = userDao.findOne(userId);
 		if ("admin".equals(user.getRoles()) || "Head".equals(user.getRoles())) {
 			filters.put("level", new SearchFilter("level", Operator.GTE, 1));
@@ -334,8 +328,7 @@ public class ActivityService {
 		return activityDao.findAll(spec, pageRequest);
 	}
 
-	public Page<Activity> getAllmyfqActivity(Long userId, Map<String, Object> searchParams, int pageNumber,
-			int pageSize, String sortType) {
+	public Page<Activity> getAllmyfqActivity(Long userId, Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) {
 		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
 		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
 		filters.put("isdelete", new SearchFilter("isdelete", Operator.EQ, "0"));
--- src/main/webapp/WEB-INF/views/activity/activityForm.jsp
+++ src/main/webapp/WEB-INF/views/activity/activityForm.jsp
@@ -65,6 +65,13 @@
 							<option value="≤400">≤400</option> 
 							<option value="≤500">≤500</option> 
 						</shiro:hasAnyRoles>
+					    <shiro:hasAnyRoles name="admin,Head">
+							<option value="≤600">≤600</option> 
+							<option value="≤700">≤700</option> 
+							<option value="≤800">≤800</option> 
+							<option value="≤900">≤900</option> 
+							<option value="≤1000">≤1000</option> 
+						</shiro:hasAnyRoles>
 					</select>
 				</div>
 			</div>
